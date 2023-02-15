4854c4b17af1ee4325d7d79f54aebb6610b4ce14
Rob Coward <rob.coward@devops-consultants.co.uk>
2023-02-15 16:19:01 +0000

    Toggle enable_nat_gateway flag

--- Feb2023/terraform/main.tf
+++ Feb2023/terraform/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
   database_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway     = false
+  enable_nat_gateway     = true
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
 
