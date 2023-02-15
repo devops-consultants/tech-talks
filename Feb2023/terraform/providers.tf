dceb201175aeef2efb08106dcc38a869bdb5ff94
Rob Coward <rob.coward@devops-consultants.co.uk>
2023-02-15 12:02:25 +0000

    Added terraform for AWS deployment

---
+++ Feb2023/terraform/providers.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 4.1"
+      source  = "hashicorp/aws"
+    }
+
+    # tls = {
+    #   version = ">= 3.0"
+    #   source  = "hashicorp/tls"
+    # }
+
+    kubernetes = {
+      version = ">= 2.10"
+      source  = "hashicorp/kubernetes"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+
+    environment = {
+      source  = "EppO/environment"
+      version = "~> 1.3"
+    }
+
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = ">= 3.4.3"
+    # }
+  }
+}
+
+data "environment_variables" "all" {}
+
+provider "aws" {
+  region = var.region
+
+  assume_role_with_web_identity {
+    role_arn           = "arn:aws:iam::${var.aws_account}:role/${var.iam_role}"
+    web_identity_token = data.environment_variables.all.items["TFC_WORKLOAD_IDENTITY_TOKEN"]
+  }
+
+  default_tags {
+    tags = {
+      Created_by = "Terraform"
+      Project    = "TechTalks"
+    }
+  }
+}
+
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+
+#   experiments {
+#     manifest_resource = true
+#   }
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#   }
+# }
+
+# provider "kubectl" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#   load_config_file       = false
+# }
---
+++ Feb2023/terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_account" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+variable "iam_role" {
+  type        = string
+  description = "AWS IAM Role ARN to assume"
+  default     = "ciadmin"
+}
+
+variable "region" {
+  description = "The AWS Region in which you want to deploy the resources"
+  type        = string
+  default     = "eu-west-2"
+}
+
